4f1a00d3ffca2aeea61912b9e1460812eb999d6d
danteal17 <dantelaviero@gmail.com>
2025-09-12 12:20:02 -0300

    probando github actions

--- src/main/java/com/dante/holamundo/HolaMundoMod.java
+++ src/main/java/com/dante/holamundo/HolaMundoMod.java
@@ -1,21 +1,10 @@
 package com.dante.holamundo;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @Mod("holamundo")
 public class HolaMundoMod {
     public HolaMundoMod() {
         System.out.println(">>> [HolaMundo] El mod se ha cargado correctamente!");
-    }
-
-    @EventBusSubscriber(modid = "holamundo")
-    public static class Events {
-        @SubscribeEvent
-        public static void onServerStarting(ServerStartingEvent event) {
-            System.out.println(">>> [HolaMundo] Hola mundo desde el servidor de Minecraft!");
-        }
     }
 }
