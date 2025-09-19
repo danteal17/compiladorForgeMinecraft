8103797a4732285bafca49a695fbd9821a50434e
danteal17 <105029190+danteal17@users.noreply.github.com>
2025-09-19 12:39:00 -0300

    Update HolaMundoMod.java

--- src/main/java/com/dante/holamundo/HolaMundoMod.java
+++ src/main/java/com/dante/holamundo/HolaMundoMod.java
@@ -1,10 +1,28 @@
 package com.dante.holamundo;
-import com.dante.holamundo.Config;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod("holamundo")
-public class HolaMundoMod {
-    public HolaMundoMod() {
-        System.out.println(">>> [HolaMundo] El mod se ha cargado correctamente!");
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+@Mod(HolamundoMod.MODID)
+public class HolamundoMod {
+
+    public static final String MODID = "holamundo";
+
+    public HolamundoMod() {
+        // Register the setup method for modloading
+        Mod.EventBusSubscriber.Bus.MOD.bus().register(this);
+    }
+
+    @SubscribeEvent
+    public static void setup(FMLCommonSetupEvent event) {
+        // Common setup code here
+        System.out.println("Hola Mundo mod is loading!");
+    }
+
+    @SubscribeEvent
+    public static void doClientStuff(FMLClientSetupEvent event) {
+        // Client setup code here
     }
 }
