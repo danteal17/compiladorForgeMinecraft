d461fcb243c9d5028d15a47e5e216c9bf8960f23
danteal17 <105029190+danteal17@users.noreply.github.com>
2025-09-25 11:37:45 -0300

    Update HolaMundoMod.java

--- src/main/java/com/dante/holamundo/HolaMundoMod.java
+++ src/main/java/com/dante/holamundo/HolaMundoMod.java
@@ -1,24 +1,51 @@
 package com.dante.holamundo;
 
-import net.minecraftforge.fml.common.Mod;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 
 @Mod(HolaMundoMod.MODID)
 public class HolaMundoMod {
     public static final String MODID = "holamundo";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public HolaMundoMod() {
-        // El constructor queda vacío en Forge 1.21.8
-        // No se usa más getModEventBus()
+        // Get mod event bus
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+
+        // Register this class for server/game events
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Client setup via static subscriber
+        modEventBus.addListener(this::dummyClientSetup);
     }
 
-    // Suscribimos métodos estáticos al bus con @EventBusSubscriber
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ModEvents {
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("¡Hola mundo desde Forge 1.21.8!");
+    }
+
+    // Dummy client listener to satisfy Forge
+    private void dummyClientSetup(FMLClientSetupEvent event) {}
+
+    @SubscribeEvent
+    public void onServerStarting(net.minecraftforge.event.server.ServerStartingEvent event) {
+        LOGGER.info("Servidor iniciando...");
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(RegisterClientReloadListenersEvent event) {
-            System.out.println("Hola mundo desde Forge 1.21.8!");
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("Cliente listo!");
         }
     }
 }
