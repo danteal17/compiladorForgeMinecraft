69e672afdcef86bc388d1a7b3350e8efb290979a
danteal17 <105029190+danteal17@users.noreply.github.com>
2025-09-25 11:24:03 -0300

    Update HolaMundoMod.java

--- src/main/java/com/dante/holamundo/HolaMundoMod.java
+++ src/main/java/com/dante/holamundo/HolaMundoMod.java
@@ -1,21 +1,22 @@
 package com.dante.holamundo;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 
 @Mod(HolaMundoMod.MODID)
-@EventBusSubscriber(modid = HolaMundoMod.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class HolaMundoMod {
     public static final String MODID = "holamundo";
 
     public HolaMundoMod() {
-        // Constructor del mod
+        // Obtenemos el EventBus de Forge
+        IEventBus modEventBus = net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Registramos nuestro listener para el ciclo de vida del cliente
+        modEventBus.addListener(this::onClientSetup);
     }
 
-    @SubscribeEvent
-    public static void onClientSetup(FMLClientSetupEvent event) {
-        System.out.println("¡Hola mundo desde Forge!");
+    private void onClientSetup(final FMLClientSetupEvent event) {
+        System.out.println("¡Hola mundo desde Forge 1.21!");
     }
 }
